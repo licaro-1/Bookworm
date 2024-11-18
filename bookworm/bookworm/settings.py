3120172f0f0f681f46bc5ee2303558eed6e0f7a6
Лишухай Дмитрий <dlishuhai@yandex.ru>
2024-11-18 08:42:35 +0300

    Update workflow

--- bookworm/bookworm/settings.py
+++ bookworm/bookworm/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 
@@ -103,33 +104,26 @@ WSGI_APPLICATION = "bookworm.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# TESTING = 'test' in sys.argv
-#
-# if TESTING:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-#             "NAME": os.getenv("DB_NAME", default="postgres"),
-#             "USER": os.getenv("POSTGRES_USER", default="admin"),
-#             "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-#             "HOST": os.getenv("DB_HOST", default="db"),
-#             "PORT": os.getenv("DB_PORT", default=5432)
-#         }
-#     }
+TESTING = 'test' in sys.argv
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if TESTING:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
+            "NAME": os.getenv("DB_NAME", default="postgres"),
+            "USER": os.getenv("POSTGRES_USER", default="admin"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
+            "HOST": os.getenv("DB_HOST", default="db"),
+            "PORT": os.getenv("DB_PORT", default=5432)
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
